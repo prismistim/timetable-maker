9de59e0e1e086376cfddb428a911fa8c704a37c5
prismistim <prismistim@hotmail.com>
2019-01-19 16:33:19 +0900

    できた

--- js/save.js
+++ js/save.js
@@ -1,12 +1,14 @@
 $(function(){
-  $('#save').click(function() {
-    html2canvas($('#result'), {
-      onrendered : function(canvas) {
-        $('#preview').empty();
-        $('#preview').append(canvas);
-      }
+  $('#save').on('click', function() {
+    html2canvas(document.querySelector('#result')).then(canvas => {
+      var image = canvas.toDataURL('image/png');
+
+      $('#dl').attr("download", "timetable.png")
+        .attr("href", image);  
+      
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      document.getElementById("dl").dispatchEvent(evt);
     });
   });
-
-  $()
-})
+});
